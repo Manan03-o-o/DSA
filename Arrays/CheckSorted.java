aec68f47ff9aa00cf0dd4c5b6ce58a53c0cb0387
Manan <manannagpal003@gmail.com>
2026-07-21 13:09:16 +0530

    added check if an array is sorted

---
+++ Arrays/CheckSorted.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class CheckSorted {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+
+        for(int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        boolean sorted = true;
+
+        for(int i = 1; i < n; i++) {
+
+            if(arr[i] < arr[i - 1]) {
+                sorted = false;
+                break;
+            }
+        }
+
+        if(sorted)
+            System.out.println("Sorted");
+        else
+            System.out.println("Not Sorted");
+    }
+}
