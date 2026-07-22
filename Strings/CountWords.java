c23b5e7022f427fd5bee59b70053b47543c28a6e
Manan <manannagpal003@gmail.com>
2026-07-22 13:23:21 +0530

    added count words

---
+++ Strings/CountWords.java
@@ -0,0 +1,22 @@
+package Strings;
+
+import java.util.Scanner;
+
+public class CountWords {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        String str = sc.nextLine().trim();
+
+        if(str.isEmpty()) {
+            System.out.println(0);
+            return;
+        }
+
+        String[] words = str.split("\\s+");
+
+        System.out.println("Words = " + words.length);
+    }
+}
